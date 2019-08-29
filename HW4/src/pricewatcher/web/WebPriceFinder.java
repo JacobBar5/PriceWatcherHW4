8f3bd17144f41e87ee3dd521fe957eca92fcbb4c
jacob <jacob@DESKTOP-RC7QRT1>
2019-08-29 12:33:13 -0600

    Changed comments

--- HW4/src/pricewatcher/web/WebPriceFinder.java
+++ HW4/src/pricewatcher/web/WebPriceFinder.java
@@ -45,10 +45,17 @@ public class WebPriceFinder extends pricefinder{
 		  Pattern bestBuy = Pattern.compile("Your price for this item is (\\$)(\u003c!-- -->)(\\d+\\.\\d+)");
 		  Pattern amazon = Pattern.compile(".a-size-medium a-color-price priceBlockBuyingPriceString\">(\\$)(\\d+\\.\\d+)");
 		  Pattern ebay = Pattern.compile("<span class=\"notranslate\" id=\"prcIsum\" itemprop=\"price\"\\s*style=\"\" content=\"(\\d+\\.\\d+)");
+		  Pattern utep = Pattern.compile("<span style=\"color: blue\">(\\$)(\\d+\\.+\\d+)</span>"); // </span>
 		  Matcher m; 
 
 		  while ((line = in.readLine()) != null) {	  
 			  //URLConnection urlc=url.openConnection();
+			  m = utep.matcher(line);
+			  if (m.find()) {
+			        output = m.group(4);
+			        return Float.valueOf(output);
+			    }
+			  
 			   m = bestBuy.matcher(line);
 			  if (m.find()) {
 			        output = m.group(3);
@@ -100,6 +107,7 @@ public class WebPriceFinder extends pricefinder{
 // - Security Lights:	https://www.amazon.com/dp/B078P8LKTX/?coliid=I3KZEVBEU26Y7H&colid=7FH4WLYA9ET4&psc=0&ref_=lv_ov_lig_dp_it
 //   Google Home Hub: 	https://www.ebay.com/itm/Google-Home-Hub-with-Google-Assistant-GA00516-US-w-Nest-Wi-Fi-Video-Doorbell/323561077867?_trkparms=5373%3A5000006400%7C5374%3ATech%7C5079%3A5000006400
 //	 Heated Blanket:	https://www.amazon.com/dp/B0088AHJ76/ref=cm_gf_aQU_i6_d_c0_qd0_______________________ONArFiuxAeBGVF6YpPsi
+//	 Utep Processor:	http://www.cs.utep.edu/cheon/cs3331/homework/hw4/
 	
 	
 //	public void validVer() {
@@ -111,7 +119,7 @@ public class WebPriceFinder extends pricefinder{
 	}
 	 
 
-/* 		old code
+/* 		old code.
 
 package pricewatcher.web;
 
